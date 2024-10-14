b996f751d9d61914b906a431be3df3e88f4fdbab
Dan Villarreal <d.vill@pitt.edu>
2024-10-14 19:04:23 -0400

    Add design-decisions as another collapsible callout

--- assets/js/collapse-callouts.js
+++ assets/js/collapse-callouts.js
@@ -1,16 +1,16 @@
-/* try-it.js 
+/* collapse-callouts.js 
 	 
-	 Make "Try it!" callouts collapsible
+	 Make "Try it!" and "Design decisions" callouts collapsible
 */
 
-function tryit(node) {
+function collapsible(node) {
   //Create new details-summary node to replace current try-it
-	var newTI = document.createElement("blockquote"),
+	var newCallout = document.createElement("blockquote"),
 			d = document.createElement("details"),
 			s = document.createElement("summary"),
 			nodeCopy = node.cloneNode(true);
 	d.appendChild(s);
-	newTI.appendChild(d);
+	newCallout.appendChild(d);
 	
 	//Move contents and class to details node
 	if (nodeCopy.children.length === 0) {
@@ -18,25 +18,27 @@ function tryit(node) {
 	} else {
 		Array.from(nodeCopy.children).map(c => d.appendChild(c));
 	}
-	newTI.classList = nodeCopy.classList;
+	newCallout.classList = nodeCopy.classList;
 	
 	//Add title: either default title or from first paragraph
-	if (!newTI.classList.contains("try-it-title")) {
-	  s.textContent = "Try it!";
-	} else {
+	if (newCallout.classList.contains("try-it-title")) {
 	  titleNode = d.querySelector("p");
 	  s.textContent = titleNode.innerText;
 	  titleNode.remove();
+	} else if (newCallout.classList.contains("try-it")) {
+	  s.textContent = "Try it!";
+	} else if (newCallout.classList.contains("design-decisions")) {
+	  s.textContent = "Design decisions";
 	}
 	
 	//Remove copy of original node
 	nodeCopy.remove();
 	
 	//Add new node in place of original
-	node.parentElement.replaceChild(newTI, node);
+	node.parentElement.replaceChild(newCallout, node);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-	var nodesTryIt = document.querySelectorAll(":is(.try-it, .try-it-title)");
-	nodesTryIt.forEach(tryit);
+	var callouts = document.querySelectorAll(":is(.try-it, .try-it-title, .design-decisions)");
+	callouts.forEach(collapsible);
 });
