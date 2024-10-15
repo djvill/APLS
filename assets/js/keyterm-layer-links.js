574b996e45495a8cffeac03c6f5a87a777b3fea1
Dan Villarreal <d.vill@pitt.edu>
2024-10-15 14:53:33 -0400

    Add code for keyterms, styling for keyterms & layers

---
+++ assets/js/keyterm-layer-links.js
@@ -0,0 +1,39 @@
+/* keyterm-layer-links.js
+   
+   Turn keyterm/layer <span>s into links by adding <a> child
+   
+   !!TODO:
+   For keyterms, facilitate "In context" backlinks (in doc/glossary) by adding
+   keyterm-<TERM> ID to:
+   - The token of <TERM> with class term-anchor (if any), OR
+   - The first token of <TERM> (if none are .term-anchor)
+   This facilitates pointing the backlink to a specific token.
+   If >1 token of <TERM> is .term-anchor, an error is thrown
+*/
+
+function addA(node, type) {
+  //Create new <a> as child of node and move text to <a>
+  var nodeText = node.innerText,
+      a = document.createElement("a");
+  a.innerText = nodeText;
+  node.innerText = '';
+  node.appendChild(a);
+  
+  //Add href depending on type
+  if (type === "keyterm") {
+    var page = "/APLS/doc/glossary";
+  } else if (type === "layer") {
+    var page = "/APLS/doc/layer-reference";
+  }
+  //Normalize name
+  var hrefName = nodeText.replaceAll(" ", "-").toLowerCase();
+  a.href = page + "#" + hrefName;
+}
+
+//Add anchors once page loads
+window.addEventListener('DOMContentLoaded', () => {
+  var keyterms = document.querySelectorAll(".keyterm"),
+      layers = document.querySelectorAll(".layer");
+  keyterms.forEach(a => addA(a, "keyterm"));
+  layers.forEach(a => addA(a, "layer"));
+});
--- doc/about/glossary.md
+++ doc/about/glossary.md
@@ -4,9 +4,19 @@ layout: default
 permalink: /doc/glossary
 parent: About APLS
 nav_order: 40
-last_modified_date: 2024-02-07T10:11:03-05:00
+last_modified_date: 2024-10-15T14:53:33-04:00
 ---
 
 # {{ page.title }}
 {:.no_toc}
 
+<style>
+  <!-- Style page-internal links like .keyterm -->
+  a[href^="#"] {
+    font-weight: bold;
+  }
+  <!-- But not in the TOC -->
+  ol#markdown-toc a {
+    font-weight: normal;
+  }
+</style>
