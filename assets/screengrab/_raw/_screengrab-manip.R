ece7c5255669b6894b57982b6aac3de14cdbaeaf
Dan Villarreal <d.vill@pitt.edu>
2024-12-04 14:23:11 -0500

    Screenshot scripting: Add functionality for overlaying images like pointers

--- assets/screengrab/_raw/_screengrab-manip.R
+++ assets/screengrab/_raw/_screengrab-manip.R
@@ -16,20 +16,48 @@ suppressWarnings(suppressPackageStartupMessages(library(magick)))
 
 ##Check that all arguments are files defined in manip_key
 to_manip <- commandArgs(trailingOnly = TRUE)
-manipable <- 
-  manip_key |>
-  read_yaml() |>
-  map(discard_at, "instructions") |>
-  map_dfr(as_tibble)
-unmanipable <- setdiff(to_manip, manipable$screengrab)
+yaml <- read_yaml(manip_key)
+manipable <- map_chr(yaml, "screengrab")
+unmanipable <- setdiff(to_manip, manipable)
 if (length(unmanipable) > 0) {
   stop("The following file(s) are not defined in ", manip_key, ": ", paste(unmanipable, collapse=" "))
 }
 
-##Transform and write
-manipable |>
-  filter(screengrab %in% to_manip) |>
-  mutate(geometry = paste0(width, "x", height, "+", offset_x, "+", offset_y),
-         in_image = map(screengrab, image_read),
-         out_image = map2(in_image, geometry, image_crop)) |>
-  with(walk2(out_image, file.path(out_dir, screengrab), image_write))
+##Wrangle YAML to tibble (with list-column for overlay, if applicable)
+manip <-
+  yaml |>
+  keep(~ .x$screengrab %in% to_manip) |>
+  map_dfr(~ .x |>
+            discard_at("instructions") |>
+            modify_at("overlay", ~ list(map_dfr(.x, as_tibble))) |>
+            as_tibble()) |>
+  ##Set up images & paths
+  mutate(in_image = map(screengrab, image_read),
+         out_image = in_image,
+         out_path = file.path(out_dir, screengrab))
+
+##Optionally overlay additional images
+if ("overlay" %in% colnames(manip)) {
+  manip <- manip |>
+    mutate(out_image = map2(out_image, overlay, \(img, ov) {
+      if (is.null(ov)) {
+        ##If no overlay, return out_image
+        img
+      } else {
+        ##If overlay, overlay images in order
+        reduce2(.init=img, map(ov$image, image_read), 
+                geometry_point(ov$offset_x, ov$offset_y),
+                \(x, y, z) image_composite(x, y, offset=z))
+      }}
+    ))
+}
+
+##Crop
+manip <- manip |>
+  mutate(geometry = geometry_area(width, height, offset_x, offset_y), ##paste0(width, "x", height, "+", offset_x, "+", offset_y)
+         .by=screengrab) |>
+  mutate(out_image = map2(out_image, geometry, image_crop))
+
+##Write
+with(manip, 
+     walk2(out_image, out_path, image_write))
