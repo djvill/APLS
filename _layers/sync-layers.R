976388dcb98e888f2c9e7d65e80b48bf38863c59
Dan Villarreal <d.vill@pitt.edu>
2025-03-24 12:49:26 -0400

    Spoof alignments for turn/word/segment

--- _layers/sync-layers.R
+++ _layers/sync-layers.R
@@ -27,6 +27,9 @@ update_existing_js_file <- TRUE
 ##Path to file for writing session info (NULL to skip)
 session_info <- "_session-info_sync-layers.txt"
 
+##Whether to show turn/word/segment as 'complete interval' alignment
+##  See https://github.com/djvill/labbcat-server/commit/2af6f41
+spoof_alignment <- TRUE
 
 # Preliminaries -----------------------------------------------------------
 
@@ -132,13 +135,6 @@ layers <- layers |>
            data_type=="number" ~ "numeric",
            data_type=="string" ~ "text")),
          across(alignment, ~ case_when(
-           ##turn/word/segment *technically* have alignment=2 because that's
-           ##  relative to their parents (participant/turn/word), but
-           ##  *conceptually* they correspond to a "full" phrase/word/segment
-           ##However, LaBB-CAT uses the underlying alignment to style layer
-           ##  icons, so for now, keep this as-is.
-           # id %in% c("turn","word","segment") ~ "complete interval",
-           
            ##Same story for phrase layers---LaBB-CAT uses the sub-intervals
            ##  icon; for exporting matches to CSV, it treats phrase layers as
            ##  though they can't have horizontal peers (and actually, ditto for
@@ -208,6 +204,13 @@ layers <- layers |>
            .default=FALSE),
          .before=extra)
 
+##Optionally spoof alignments for turn/word/segment
+if (spoof_alignment) {
+  layers <- layers |>
+    mutate(across(alignment, ~ if_else(id %in% c("turn","word","segment"), 
+                                       "complete interval",
+                                       alignment)))
+}
 
 ## Wrangle attributes ======================================================
 
--- _layers/word.md
+++ _layers/word.md
@@ -1,9 +1,9 @@
 ---
 synced:
+  layer_id: 0
   short_description: The word as originally transcribed, including pause/question
     markers
-  layer_id: 0
-  alignment: sub-interval
+  alignment: complete interval
   project: (none)
   parent: turn
   peers: yes
@@ -19,7 +19,7 @@ synced:
   export_includeCounts: no
   export_includeAnchorSharing: no
   extra: (none)
-last_sync_modified_date: 2025-02-05T16:45:13-0500
+last_sync_modified_date: 2025-03-24T12:46:32-0400
 notation:
   primary: English spelling
   additional:
@@ -33,7 +33,7 @@ alignment_dependent: no
 versions:
   first_appeared: 0.1.0
   last_modified: 0.1.0
-last_modified_date: 2025-03-22T14:13:33-04:00
+last_modified_date: 2025-03-24T12:49:27-04:00
 ---
 
 **Fill longer description here**
